ae56f63990dc58a78a85b97eccd7640579c3fcd7
sunilhmistry <sunilmistry07@gmail.com>
2017-04-27 17:45:46 +0530

    Revert "Add files via upload"
    This reverts commit ec74d53d8047d07f05c90faf9e4809c8dab5a851.

--- if.py
+++
@@ -1,7 +0,0 @@
-count=0
-
-while count <5:
-    print count, "Is less than 5"
-    count = count +1
-else:
-    print count, "is equal to 5"
--- var1.py
+++
@@ -1,9 +0,0 @@
-list = ['abcd', 123, 2.58, 'Vaidehi', 'SUN05']
-tinylist = ['Mistry']
-
-print list
-print list[0]
-print list [1:3]
-print list [3:]
-print tinylist* 3
-print list + tinylist
